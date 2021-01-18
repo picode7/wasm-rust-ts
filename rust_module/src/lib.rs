8de04f6b8d59e59d024a621480243f0908c08a97
Pierre Siebentritt <pierre.siebentritt@motius.de>
2021-01-18 18:30:59 +0100

    added benchmark sum_up and wasm array

--- rust_module/src/lib.rs
+++ rust_module/src/lib.rs
@@ -6,5 +6,23 @@ pub fn add(n1: f64, n2: f64) -> f64 {
     for _i in 0..100_000_000 {
         n += n2;
     }
-    n 
+    n
+}
+
+#[wasm_bindgen]
+pub fn sum_up(a: &mut [f64]) -> f64 {
+    let mut sum: f64 = 0.0;
+    for v in a.iter() {
+        sum += v;
+    }
+    sum
+}
+
+#[wasm_bindgen]
+pub fn init_arr() -> *const f64 {
+    let mut my_array: Vec<f64> = Vec::with_capacity(100_000_000);
+    for i in 0..my_array.len() {
+        my_array[i] = i as f64;
+    }
+    my_array.as_ptr()
 }
--- src/init.ts
+++ src/init.ts
@@ -1,19 +1,75 @@
-import * as wasm from "../rust_module/pkg";
+import * as wasm from "../rust_module/pkg/rust_module";
+import { memory } from "../rust_module/pkg/rust_module_bg.wasm";
 
 function add(a: number, b: number) {
   let n = a;
-  for (let i = 0; i < 100000000; ++i) n += b;
+  for (let i = 0; i < 100_000_000; ++i) n += b;
   return n;
+}
+
+function sum_up(arr: Float64Array) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; ++i) {
+    sum += arr[i];
+  }
+  return sum;
+}
+
+function init_arr() {
+  let arr = new Float64Array(100_000_000);
+  for (let i = 0; i < arr.length; ++i) {
+    arr[i] = i;
+  }
+  return arr;
 }
 
 {
   let start = performance.now();
   let result = wasm.add(2, 8);
-  console.log("wasm", performance.now() - start, "ms", result);
+  console.log("add wasm", performance.now() - start, "ms", result);
 }
 
 {
   let start = performance.now();
   let result = add(2, 8);
-  console.log("js", performance.now() - start, "ms", result);
+  console.log("add js", performance.now() - start, "ms", result);
+}
+
+let start2 = performance.now();
+let ptr = wasm.init_arr();
+let wasmArr = new Float64Array(memory.buffer, ptr, 100_000_000);
+console.log("init wasm-arr", performance.now() - start2, "ms");
+
+let start1 = performance.now();
+let arr = init_arr();
+console.log("init js-arr", performance.now() - start1, "ms");
+
+{
+  let start = performance.now();
+  let result = wasm.sum_up(new Float64Array(arr));
+  console.log("sum_up js-arr", "wasm", performance.now() - start, "ms", result);
+}
+
+{
+  let start = performance.now();
+  let result = sum_up(new Float64Array(arr));
+  console.log("sum_up js-arr", "js", performance.now() - start, "ms", result);
+}
+
+{
+  let start = performance.now();
+  let result = wasm.sum_up(wasmArr);
+  console.log(
+    "sum_up wsm-arr",
+    "wasm",
+    performance.now() - start,
+    "ms",
+    result
+  );
+}
+
+{
+  let start = performance.now();
+  let result = sum_up(wasmArr);
+  console.log("sum_up wsm-arr", "js", performance.now() - start, "ms", result);
 }
